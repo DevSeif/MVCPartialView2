bd01e184f5db4ca903cc06d024d8a58a027f1681
DevSeif <65452750+DevSeif@users.noreply.github.com>
2022-11-09 11:08:23 +0100

    Add files via upload

---
+++ PeopleMVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleMVC.Models;
+using System.Diagnostics;
+
+namespace PeopleMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(PeopleViewModel.people);
+        }
+
+        [HttpGet]
+        public IActionResult Index(string filterWord)
+        {
+
+            List<Person> filterList = new List<Person>();
+
+            if (filterWord == null || filterWord.Trim() == "") { return View(PeopleViewModel.people); }
+
+            foreach (Person person in PeopleViewModel.people)
+            {
+                if (person.City.Contains(filterWord) || person.Name.Contains(filterWord))
+                {
+                    filterList.Add(person);
+                }
+            }
+
+
+            return View(filterList);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreatePersonViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                PeopleViewModel.people.Add(new Person(model.Name, model.PhoneNumber, model.City));   
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult PersonPartial()
+        {
+            return PartialView("_Person", PeopleViewModel.people);
+        }
+
+    }
+}
---
+++ PeopleMVC/Models/CreatePersonViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PeopleMVC.Models
+{
+    public class CreatePersonViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(13)]
+        [MinLength(8)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string City { get; set; }
+    }
+}
